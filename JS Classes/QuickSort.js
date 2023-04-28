25c26eb556a5374cdc2fd5fa33a1a2950220ada6
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-04-28 15:36:05 +0300

    Update QuickSort.js

--- JS Classes/QuickSort.js
+++ JS Classes/QuickSort.js
@@ -1,15 +1,22 @@
+// Клас, който дефинира статичен метод sort, извършващ quick sort върху подаден масив
 class QuickSort {
+    // Статичен метод, който приема масив и връща сортиран масив, използвайки quick sort
     static sort(array) {
+        // Ако масивът има дължина по-малка или равна на 1, просто го връщаме
         if (array.length <= 1) {
             return array;
         }
 
+        // Избираме произволен елемент на масива като пивот
         const pivotIndex = Math.floor(array.length / 2);
         const pivot = array[pivotIndex];
+        // Инициализираме два нови масива - един за елементите, по-малки от пивота, и един за елементите, по-големи от него
         const less = [];
         const greater = [];
 
+        // Обхождаме масива и разделяме всички елементи в подходящите масиви
         for (let i = 0; i < array.length; i++) {
+            // Пропускаме пивотния елемент
             if (i === pivotIndex) {
                 continue;
             }
@@ -23,10 +30,14 @@ class QuickSort {
             }
         }
 
+        // Рекурсивно сортираме двата нови масива и ги обединяваме заедно с пивота, за да получим крайния сортиран масив
         return [...QuickSort.sort(less), pivot, ...QuickSort.sort(greater)];
     }
 }
 
+// Създаваме масив, който искаме да сортираме
 const unsortedArray = [3, 7, 8, 5, 2, 1, 9, 5, 4];
+// Използваме метода sort на класа QuickSort, за да сортираме масива
 const sortedArray = QuickSort.sort(unsortedArray);
+// Извеждаме сортирания масив на конзолата
 console.log(sortedArray);
